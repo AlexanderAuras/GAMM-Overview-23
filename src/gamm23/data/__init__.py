025139dee6d903216321fc6a9142f00727d76018
Alexander Auras <33501357+AlexanderAuras@users.noreply.github.com>
2024-01-12 16:03:34 +0100

    Fixes

--- src/gamm23/data/__init__.py
+++ src/gamm23/data/__init__.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-
 from typing import Tuple, Union, cast
 
 import torch
@@ -7,11 +6,9 @@ from torch import Tensor
 from torch.utils.data import Dataset, TensorDataset
 
 
-
-def load_datasets(base_path: Union[str,Path]) -> Tuple[Dataset[Tuple[Tensor,Tensor]],Dataset[Tuple[Tensor,Tensor]],Dataset[Tuple[Tensor,Tensor]]]:
+def load_datasets(base_path: Union[str, Path]) -> Tuple[Dataset[Tuple[Tensor, Tensor]], Dataset[Tuple[Tensor, Tensor]], Dataset[Tuple[Tensor, Tensor]]]:
     base_path = Path(base_path)
     train_dataset = TensorDataset(*torch.load(str(base_path.joinpath("train.pth").resolve())))
     val_dataset = TensorDataset(*torch.load(str(base_path.joinpath("val.pth").resolve())))
     test_dataset = TensorDataset(*torch.load(str(base_path.joinpath("test.pth").resolve())))
-    return cast(Tuple[Dataset[Tuple[Tensor,Tensor]],Dataset[Tuple[Tensor,Tensor]],Dataset[Tuple[Tensor,Tensor]]], (train_dataset, val_dataset, test_dataset))
-
+    return cast(Tuple[Dataset[Tuple[Tensor, Tensor]], Dataset[Tuple[Tensor, Tensor]], Dataset[Tuple[Tensor, Tensor]]], (train_dataset, val_dataset, test_dataset))
